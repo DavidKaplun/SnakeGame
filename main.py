721e490daa2c03ee488b49d6a0d4f1b585d72cc5
DavidKaplun <davidkaplun505@gmail.com>
2024-01-29 04:16:09 +0200

    the wall is being generated correctly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from snake import snake
-from random import randint
+import random
 from block import block
 from constants import *
 from wall import *
@@ -11,6 +11,7 @@ gameDisplay.fill(WHITE)
 def main():
     score=0
     global my_snake
+    global apple
     my_snake=snake()
     apple = create_apple()
     prev_dir=""
@@ -21,13 +22,13 @@ def main():
 
         while apple==APPLE_INSIDE_SNAKE or apple==EATEN_APPLE:
             apple=create_apple()
-            wall = generate_wall(apple, my_snake.get_head())
+            wall = generate_wall()
 
-        draw_apple(apple.get_pos_x(),apple.get_pos_y())
+        draw_apple()
         draw_wall(wall)
 
 
-        if snake_eating_apple(apple.get_pos_x(),apple.get_pos_y()):
+        if snake_eating_apple():
             prev_dir = my_snake.get_last_block().get_direction()
             my_snake.grow()
             apple=EATEN_APPLE
@@ -43,22 +44,15 @@ def main():
                 last_snake_block.turn(prev_dir)
         pygame.time.delay(TIME_DELAY)
 
-def draw_wall(wall):
-    count=0
-    for block in wall:
-        count+=1
-        pygame.draw.rect(gameDisplay, WALL_COLOR, [block.get_pos_x(), block.get_pos_y(), SQUARE_SIZE, SQUARE_SIZE])
-    print(count)
-
-def snake_eating_apple(apple_x,apple_y):
+def snake_eating_apple():
     snake_head=my_snake.get_head()
-    apple_rect=pygame.Rect(apple_x,apple_y,SQUARE_SIZE,SQUARE_SIZE)
+    apple_rect=pygame.Rect(apple.get_pos_x(),apple.get_pos_y(),SQUARE_SIZE,SQUARE_SIZE)
     snake_head_rect=pygame.Rect(snake_head.get_pos_x(),snake_head.get_pos_y(),SQUARE_SIZE,SQUARE_SIZE)
     return apple_rect.colliderect(snake_head_rect)
 
 def create_apple():
-    apple_x=randint(0,BOARD_LENGTH-1)*SQUARE_SIZE
-    apple_y=randint(0,BOARD_HEIGHT-1)*SQUARE_SIZE
+    apple_x=random.randint(0,BOARD_LENGTH-1)*SQUARE_SIZE
+    apple_y=random.randint(0,BOARD_HEIGHT-1)*SQUARE_SIZE
     if block_inside_snake(apple_x,apple_y):
         return APPLE_INSIDE_SNAKE
     return block(apple_x,apple_y)
@@ -116,7 +110,124 @@ def draw_board():
                 pygame.draw.rect(gameDisplay, BLACK, [SQUARE_SIZE * y, SQUARE_SIZE * x, SQUARE_SIZE, SQUARE_SIZE])
             color += 1
 
-def draw_apple(apple_x,apple_y):
-    pygame.draw.rect(gameDisplay, APPLE_COLOR, [apple_x, apple_y, SQUARE_SIZE, SQUARE_SIZE])
+def draw_apple():
+    pygame.draw.rect(gameDisplay, APPLE_COLOR, [apple.get_pos_x(), apple.get_pos_y(), SQUARE_SIZE, SQUARE_SIZE])
+
+def draw_wall(wall):
+    for block in wall:
+        pygame.draw.rect(gameDisplay, WALL_COLOR, [block.get_pos_x(), block.get_pos_y(), SQUARE_SIZE, SQUARE_SIZE])
+
+#below only the functions related to wall
+
+def chose_wall_type():
+    snake_head_x,snake_head_y=my_snake.get_head_x(),my_snake.get_head_y()
+    difference_x,difference_y=snake_head_x-apple.get_pos_x(),snake_head_y-apple.get_pos_y()
+    if abs(difference_x)>abs(difference_y):#if the horizontal distance bigger than vertical then build vertical walls
+        if difference_x>0:#because if its possitive then the snake is right from the apple
+            return "right"
+        return "left"
+
+    else:
+        if difference_y>0:#means snake below apple. so build a wall below apple
+            return "down"
+        return "up"
+
+def generate_wall():
+    apple_x,apple_y=apple.get_pos_x(),apple.get_pos_y()
+    wall_type=chose_wall_type()
+    wall=[]
+    #there should be a function for deciding the starting point based on apple cords
+    match wall_type:
+        case "up":
+            cur_point = block(apple_x + (2*SQUARE_SIZE), apple_y - (2*SQUARE_SIZE))
+            cur_point_x, cur_point_y = cur_point.get_pos_x(), cur_point.get_pos_y()
+            wall.append(cur_point)
+            for z in range(4):#there has to be a constant called wall length
+                option_to_expand = gen_new_block_position_options(cur_point_x, cur_point_y, "left")#should get the block not its x and y
+                new_block = chose(option_to_expand)
+                if new_block!=INVALID_BLOCK:
+                    wall.append(new_block)
+                    cur_point_x, cur_point_y = new_block.get_pos_x(), new_block.get_pos_y()
+                else:
+                    break
+
+        case "down":
+            cur_point = block(apple_x - (2*SQUARE_SIZE), apple_y + (2*SQUARE_SIZE))
+            cur_point_x, cur_point_y = cur_point.get_pos_x(), cur_point.get_pos_y()
+            wall.append(cur_point)
+            for z in range(4):  # there has to be a constant called wall length
+                option_to_expand = gen_new_block_position_options(cur_point_x, cur_point_y,"right")  # should get the block not its x and y
+                new_block = chose(option_to_expand)
+                if new_block != INVALID_BLOCK:
+                    wall.append(new_block)
+                    cur_point_x, cur_point_y = new_block.get_pos_x(), new_block.get_pos_y()
+                else:
+                    break
+
+        case "left":
+            cur_point = block(apple_x - (2*SQUARE_SIZE), apple_y + (2*SQUARE_SIZE))
+            cur_point_x, cur_point_y = cur_point.get_pos_x(), cur_point.get_pos_y()
+            wall.append(cur_point)
+            for z in range(4):  # there has to be a constant called wall length
+                option_to_expand = gen_new_block_position_options(cur_point_x, cur_point_y, "up")  # should get the block not its x and y
+                new_block = chose(option_to_expand)
+                if new_block != INVALID_BLOCK:
+                    wall.append(new_block)
+                    cur_point_x,cur_point_y=new_block.get_pos_x(), new_block.get_pos_y()
+                else:
+                    break
+
+        case "right":
+            cur_point = block(apple_x + (2*SQUARE_SIZE), apple_y - (2*SQUARE_SIZE))
+            cur_point_x,cur_point_y=cur_point.get_pos_x(), cur_point.get_pos_y()
+            wall.append(cur_point)
+            for z in range(4):  # there has to be a constant called wall length
+                option_to_expand = gen_new_block_position_options(cur_point_x,cur_point_y,"down")  # should get the block not its x and y
+                new_block = chose(option_to_expand)
+                if new_block != INVALID_BLOCK:
+                    wall.append(new_block)
+                    cur_point_x,cur_point_y=new_block.get_pos_x(), new_block.get_pos_y()
+                else:
+                    break
+    return wall
+
+def chose(pos_options):#better name for options
+    valid_options=[]
+    for option in pos_options:
+        if valid(option):
+            valid_options.append(option)
+
+    num_of_valid_options=len(valid_options)
+    if num_of_valid_options==0:
+        return INVALID_BLOCK
+    return  random.choice(valid_options)
+
+def valid(pos_option):
+    return  not block_inside_snake(pos_option.get_pos_x(),pos_option.get_pos_y()) and not block_inside_apple(pos_option)#fix this later
+
+
+def block_inside_apple(block):
+    return block.get_pos_x()==apple.get_pos_x() and block.get_pos_y()==apple.get_pos_y()
+
+def gen_new_block_position_options(block_x, block_y, build_direction):
+    new_block_position_options=[]
+    match build_direction:
+        case "up":
+            for new_x in range(block_x-SQUARE_SIZE,block_x+SQUARE_SIZE,SQUARE_SIZE):
+                new_block_position_options.append(block(new_x,block_y-SQUARE_SIZE))
+
+        case "down":
+            for new_x in range(block_x-SQUARE_SIZE,block_x+SQUARE_SIZE,SQUARE_SIZE):
+                new_block_position_options.append(block(new_x,block_y+SQUARE_SIZE))
+
+        case "right":
+            for new_y in range(block_y - SQUARE_SIZE, block_y + SQUARE_SIZE,SQUARE_SIZE):
+                new_block_position_options.append(block(block_x+SQUARE_SIZE, new_y))
+
+        case "left":
+            for new_y in range(block_y - SQUARE_SIZE, block_y + SQUARE_SIZE,SQUARE_SIZE):
+                new_block_position_options.append(block(block_x-SQUARE_SIZE, new_y))
+
+    return new_block_position_options
 
 main()

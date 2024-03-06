69bbb6cb70ba8a9b26c73e3afcdac3736b955eb4
DavidKaplun <davidkaplun505@gmail.com>
2024-03-06 20:59:07 +0200

    finished chose_response_to_message_during_game function

--- server.py
+++ server.py
@@ -1,27 +1,72 @@
 import constants
 import socket
+import threading
 
 clients_sockets=[]
+clients_sockets_lock=constants.UNLOCKED
 def open_socket():
     server_running=True
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((constants.SERVER_IP, constants.SERVER_PORT))
     server.listen(constants.MAX_QUEUE_LENGTH)
+
     while server_running:
         communication_socket=server.accept()[0]
         clients_sockets.append(communication_socket)
-        
-        message_from_client = communication_socket.recv(1024).decode('utf-8')
-        communication_socket.send(f"message received".encode('utf-8'))
-        print(message_from_client)
 
-    print('what')
-
-
-
+        if len(clients_sockets)>=2 and clients_sockets_lock==constants.UNLOCKED:
+            clients_sockets_lock=constants.LOCKED
+            thread=threading.Thread(target=start_game_between_2_players, args=[])
+            thread.start()
+            thread.join()
+            clients_sockets_lock=constants.UNLOCKED
 
 
     return #the socket
+
+def start_game_between_2_players():
+    player_1_socket=clients_sockets.pop()
+    player_2_socket=clients_sockets.pop()
+
+    game_running=True
+
+    player_1_socket.send(constants.START_GAME)
+    player_2_socket.send(constants.START_GAME)
+
+    while game_running:
+        message_from_board1 = player_1_socket.recv(1024)
+        message_from_board2 = player_2_socket.recv(1024)
+
+        response_to_player_1 = chose_response_to_message_during_game(message_from_board2)
+        response_to_player_2 = chose_response_to_message_during_game(message_from_board1)
+
+        if response_to_player_1[0]!=constants.SEND_BOARD or response_to_player_2[0]!=constants.SEND_BOARD:
+            game_running=False
+
+        player_1_socket.send(response_to_player_1.encode())
+        player_2_socket.send(response_to_player_2.encode())
+
+    player_1_socket.close()
+    player_2_socket.close()
+
+
+
+def chose_response_to_message_during_game(message):
+    message_list=message.split(" ")
+
+    message_code=message_list[0]
+    other_player_board=message_list[1]
+    is_other_player_eating_apple=message_list[2]
+
+    match message_code:
+        case constants.WON_GAME:
+            return constants.LOST_GAME
+
+        case constants.LOST_GAME:
+            return constants.WON_GAME
+
+        case constants.SEND_BOARD:
+            return constants.SEND_BOARD+" "+other_player_board+" "+is_other_player_eating_apple
 def main():
     open_socket()
     m=constants.REGISTER
